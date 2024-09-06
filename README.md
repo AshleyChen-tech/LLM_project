3943606d81466c90183dc6515596d120d95a1fae
Ashley <791752221@qq.com>
2024-09-06 21:14:51 +0800

    adding new content

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # LLM_project
 Personal practice open source project
+
+# chapter02
+This section deals with text data 
+and is used to benchmark different byte pair 
+encoder implementations.
--- chapter02/part1.py
+++ chapter02/part1.py
@@ -82,3 +82,68 @@ print(content)
 en_comtent = tokenizer.decode(tokenizer.encode(text))
 print(en_comtent)
 
+
+# # wrong example
+# tokenizer = SimpleTokenizerV1(vocab)
+# text = "Hello, do you like tea. Is this-- a test?"
+# tokenizer.encode(text)
+
+
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>", "<|unk|>"])
+
+vocab = {token:integer for integer, token in enumerate(all_tokens)}
+print(len(vocab.items()))
+for i, item in enumerate(list(vocab.items())[-5:]):
+    print(item)
+
+
+class SimpleTokenizerV2:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
+
+        preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [
+            item if item in self.str_to_int
+            else "<|unk|>" for item in preprocessed
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+
+    def decode(self, ids):
+        text = " ".join([self.int_to_str[i] for i in ids])
+        # Replace spaces before the specified punctuations
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+
+
+tokenizer = SimpleTokenizerV2(vocab)
+text1 = "Hello, do you like tea?"
+text2 = "In the sunlit terraces of the palace."
+
+text = " <|endoftext|> ".join((text1, text2))
+print(text)
+print(tokenizer.encode(text))
+print(tokenizer.decode(tokenizer.encode(text)))
+
+
+import importlib
+import tiktoken
+
+
+print("tiktoken version:", importlib.metadata.version("tiktoken"))
+tokenizer = tiktoken.get_encoding("gpt2")
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+     "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
+
