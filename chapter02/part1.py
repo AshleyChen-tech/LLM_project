62979c0b5434d6c98f5317c096fdccdc07c1076f
Ashley <791752221@qq.com>
2024-10-16 11:29:42 +0800

    add new content--2.7

--- chapter02/part1.py
+++ chapter02/part1.py
@@ -227,9 +227,23 @@ print(first_batch)
 second_batch = next(data_iter)
 print(second_batch)
 
-dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=4, stride=4, shuffle=False
+)
 
 data_iter = iter(dataloader)
 inputs, targets = next(data_iter)
 print("Inputs:\n", inputs)
 print("\nTargets:\n", targets)
+
+
+# 2.7
+input_ids = torch.tensor([2, 3, 5, 1])
+vocab_size = 6
+output_dim = 3
+
+torch.manual_seed(123)
+embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+print(embedding_layer.weight)
+print(embedding_layer(torch.tensor([3])))
+print(embedding_layer(input_ids))
